a1295101fce7ea3e14c2835b1862900ee70337e5
Heath Ritchie <heath.ritchie@gmail.com>
2013-10-11 13:56:49 -0400

    Give tags a method

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -3,7 +3,7 @@ class TagCloud
 # tag cloud code inspired by this article
 # http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
-  attr_reader :user, :tags, :min, :divisor
+  attr_reader :user, :min, :divisor
 
   def initialize(user, cut_off = nil)
     @user = user
@@ -14,14 +14,7 @@ class TagCloud
     # tag cloud code inspired by this article
     #  http://www.juixe.com/techknow/index.php/2006/07/15/acts-as-taggable-tag-cloud/
 
-    levels=10
-    # TODO: parameterize limit
-
-    params = [sql(@cut_off), user.id]
-    if @cut_off
-      params += [@cut_off, @cut_off]
-    end
-    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    @tags = tags
 
     max, @min = 0, 0
     @tags.each { |t|
@@ -33,10 +26,22 @@ class TagCloud
 
   end
 
+  def tags
+    unless @tags
+      params = [sql(@cut_off), user.id]
+      if @cut_off
+        params += [@cut_off, @cut_off]
+      end
+      @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
+    end
+    @tags
+  end
+
   private
 
   def sql(cut_off = nil)
     # Get the tag cloud for all tags for actions
+    # TODO: parameterize limit
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
@@ -50,6 +55,10 @@ class TagCloud
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
+  end
+
+  def levels
+    10
   end
 
 end
